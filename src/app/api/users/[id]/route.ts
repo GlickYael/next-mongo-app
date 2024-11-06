1b4f562fffd696aef557f2195cb2ff78e7ef620e
GlickYael <glick.yael3@gmail.com>
2024-11-06 09:45:56 +0200

    fix bug

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,31 +1,41 @@
 import { NextResponse } from "next/server";
-import { connectDatabase, updateDocument, deleteDocument,getDocumentById} from "@/services/mongo";
+import { connectDatabase, updateDocument, deleteDocument, getDocumentById } from "@/services/mongo";
 import { ObjectId } from "mongodb";
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+export async function PUT(request: Request, { params }: { params: Promise<{ id: string }>}) {
     const client = await connectDatabase();
     const body = await request.json();
     const { id } = await params;
-    const res = await updateDocument(client,'users',body,new ObjectId(id));
+    const res = await updateDocument(client, 'users', body, new ObjectId(id));
     return new NextResponse(
         res
     );
 }
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(request: Request, { params }: { params: Promise<{ id: string }>}) {
     const client = await connectDatabase();
     const { id } = await params;
-    const res = await deleteDocument(client,'users',new ObjectId(id));
+    const res = await deleteDocument(client, 'users', new ObjectId(id));
     return new NextResponse(
         res
     );
 }
 
-export async function GET(request: Request, { params }: { params: { id: string } }){
+// export async function GET(request: Request, { params }: { params: { id: string } }){
+//     const client = await connectDatabase();
+//     const { id } = await params;
+//     const res = await getDocumentById(client,'users',new ObjectId(id));
+//     return new NextResponse(
+//         res
+//     );
+// }
+export async function GET(
+    request: Request,
+    { params }: { params: Promise<{ id: string }> }  // Wrap params in Promise
+) {
     const client = await connectDatabase();
-    const { id } = await params;
-    const res = await getDocumentById(client,'users',new ObjectId(id));
-    return new NextResponse(
-        res
-    );
+    const { id } = await params;  // Await params here since it's a Promise
+
+    const res = await getDocumentById(client, 'users', new ObjectId(id));
+    return NextResponse.json(res);
 }
